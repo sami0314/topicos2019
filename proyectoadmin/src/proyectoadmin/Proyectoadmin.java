9a9bd1f6075389b38745ca2a06522e3117f5886c
samicaro0920 <47403025+samicaro0920@users.noreply.github.com>
2019-02-06 18:11:17 -0500

    Update Proyectoadmin.java
    se adicionó un sout version 2

--- proyectoadmin/src/proyectoadmin/Proyectoadmin.java
+++ proyectoadmin/src/proyectoadmin/Proyectoadmin.java
@@ -16,6 +16,7 @@ public class Proyectoadmin {
      */
     public static void main(String[] args) {
         System.out.println("Version 1");
+        System.out.println("Version 2.0");
         // TODO code application logic here     
     }
     
